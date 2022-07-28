af1c3223db8cde09b7752a45cc749957e13b9bc5
esaave3 <esaave3@bancodebogota.com.co>
2022-07-28 12:55:42 -0500

    decorator desing pattern

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # design-patterns-javascript-typescript
+
+## Run ts file with ts node: ts-node name-file.ts
--- decorator/ts/decorator.ts
+++ decorator/ts/decorator.ts
@@ -1,13 +1,14 @@
+//decorator resuelve la herencia multiple
 interface Component {
     getDetail(): string;
 }
 
 class ProductComponent implements Component {
     protected name: string;
-    constructor(name: string){
+    constructor(name: string) {
         this.name = name;
     }
-    public getDetail(): string{
+    public getDetail(): string {
         return `${this.name}`;
     }
 }
@@ -19,22 +20,45 @@ abstract class ProductDecorator implements Component {
     constructor(component: Component) {
         this.component = component;
     }
-    public getDetail(): string{
+    public getDetail(): string {
         return this.component.getDetail();
     }
 }
 
 // decorator 1
-class ComercialInfoProductDecorator extends ProductDecorator{
-    private tradename : string;
+class ComercialInfoProductDecorator extends ProductDecorator {
+    private tradename: string;
     private brand: string;
-    constructor(component: Component, tradename: string, brand: string){
+    constructor(component: Component, tradename: string, brand: string) {
         super(component);
         this.tradename = tradename;
         this.brand = brand;
     }
-    public getDetail(): string{
-        return `${this.tradename} ${this.brand}` + super.getDetail();
+    public getDetail(): string {
+        return `${this.tradename} ${this.brand} ` + super.getDetail();
+    }
+}
+// decorator 2
+class StoreProductDecorator extends ProductDecorator {
+    private price: number;
+    constructor(component: Component, price: number) {
+        super(component);
+        this.price = price;
+    }
+    public getDetail(): string {
+        return super.getDetail() + ` ${this.price}`;
+    }
+
+}
+
+// decorator 3
+class HTMLProductDecorator extends ProductDecorator {
+    public getDetail(): string {
+        return `<h1>Inforrmación del producto</h1>
+            <p>
+                ${super.getDetail()}
+            </p>
+        `;
     }
 }
 
@@ -42,3 +66,19 @@ class ComercialInfoProductDecorator extends ProductDecorator{
 //Component
 const productComponent = new ProductComponent('cerveza');
 console.log(productComponent.getDetail());
+
+//decorator 1 con component
+const comercialInfoProduct = new ComercialInfoProductDecorator(productComponent, 'London porter', 'Fuller s');
+console.log(comercialInfoProduct.getDetail());
+
+//decorator 2 con component
+const storeProduct = new StoreProductDecorator(productComponent, 15.5);
+console.log(storeProduct.getDetail());
+
+//decorator 2 con decorator 1 
+const storeProduct2 = new StoreProductDecorator(comercialInfoProduct, 15.5);
+console.log(storeProduct2.getDetail());
+
+// decorator 3 con decorator 2 con decorator 1 con componente
+const htmlProductDecorator = new HTMLProductDecorator(storeProduct2);
+console.log(htmlProductDecorator.getDetail());
